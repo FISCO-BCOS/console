bc72dd5780713f86b7fa66797ecf398af3f9a2ad
fqliao <fqliao@yeah.net>
2019-03-05 16:56:19 +0800

    modify message when lost connected to node

--- src/main/java/console/ConsoleImpl.java
+++ src/main/java/console/ConsoleImpl.java
@@ -123,7 +123,7 @@ public class ConsoleImpl implements ConsoleFace {
       service.run();
     } catch (Exception e) {
       System.out.println(
-          "Failed to connect blockchain, please check running status for blockchain and configruation for console.");
+          "Failed to connect to the node. Please check the node status and the console configruation.");
       close();
     }
     channelEthereumService.setChannelService(service);
