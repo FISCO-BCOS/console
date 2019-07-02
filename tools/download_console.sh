0a6ae508022369dcd12cef0b3d645eff7c7bdd6e
bxq2011hust <bxq2011hust@qq.com>
2019-07-02 19:53:03 +0800

    fix download_console

--- tools/download_console.sh
+++ tools/download_console.sh
@@ -5,10 +5,10 @@ echo "Downloading console ${version}"
 download_link=https://github.com/FISCO-BCOS/console/releases/download/v${version}/${package_name}
 
 if [ $(curl -IL -o /dev/null -s -w %{http_code}  https://www.fisco.com.cn/cdn/console/releases/download/v${version}/${package_name}) == 200 ];then
-    if [ ! $(curl -LO ${download_link} --speed-time 20 --speed-limit 1024 -m 90) ];then 
+    curl -LO ${download_link} --speed-time 30 --speed-limit 1024 -m 90 || {
         echo -e "\033[32m Download speed is too low, try https://www.fisco.com.cn/cdn/console/releases/download/v${version}/${package_name} \033[0m"
         curl -LO https://www.fisco.com.cn/cdn/console/releases/download/v${version}/${package_name}
-    fi
+    }
 else
     curl -LO ${download_link}
 fi
