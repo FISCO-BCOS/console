a58f303afbf5df8555a690d477afa6675ba5f21e
Xingqang Bai <bxq2011hust@qq.com>
2019-07-02 20:04:54 +0800

    update download_console (#138)
    * update download_console
    
    * fix download_console

--- tools/download_console.sh
+++ tools/download_console.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 version=$(curl -s https://api.github.com/repos/FISCO-BCOS/console/releases | grep "tag_name" | sort -u | tail -n 1 | cut -d \" -f 4 | sed "s/^[vV]//")
-echo "Downloading console version" ${version}
 package_name="console.tar.gz"
+echo "Downloading console ${version}"
 download_link=https://github.com/FISCO-BCOS/console/releases/download/v${version}/${package_name}
-curl -LO ${download_link}
 
+if [ $(curl -IL -o /dev/null -s -w %{http_code}  https://www.fisco.com.cn/cdn/console/releases/download/v${version}/${package_name}) == 200 ];then
+    curl -LO ${download_link} --speed-time 30 --speed-limit 1024 -m 90 || {
+        echo -e "\033[32m Download speed is too low, try https://www.fisco.com.cn/cdn/console/releases/download/v${version}/${package_name} \033[0m"
+        curl -LO https://www.fisco.com.cn/cdn/console/releases/download/v${version}/${package_name}
+    }
+else
+    curl -LO ${download_link}
+fi
 tar -zxf ${package_name} && cd console && chmod +x *.sh
