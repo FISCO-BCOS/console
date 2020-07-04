6d0fdbed6ed76a705e29d8329e663bb8ca94f011
chaychen2005 <605966652@qq.com>
2020-07-04 22:35:38 +0800

    write escrow data to txt

--- src/main/java/console/data/tools/SafeKeeperUrl.java
+++ src/main/java/console/data/tools/SafeKeeperUrl.java
@@ -2,13 +2,15 @@ package console.data.tools;
 
 public class SafeKeeperUrl {
 
-    public SafeKeeperUrl(String url) {
-        this.url = url;
-    }
+    private String url;
+    private String server;
 
     public SafeKeeperUrl() {}
 
-    private String url;
+    public SafeKeeperUrl(String url, String server) {
+        this.url = url;
+        this.server = server;
+    }
 
     public String getUrl() {
         return url;
@@ -16,5 +18,13 @@ public class SafeKeeperUrl {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
     }
 }
