7cfbfbc438356d1264a2012beea818ef3e13c826
octopus <912554887@qq.com>
2020-07-09 14:13:18 +0800

    update setSystemConfigKey -h info

--- src/main/java/console/common/HelpInfo.java
+++ src/main/java/console/common/HelpInfo.java
@@ -714,7 +714,7 @@ public class HelpInfo {
                         + Common.TxGasLimitRange
                         + "(default 300000000).");
         System.out.println(
-                "      -- the value of  rpbft_epoch_block_num from 1 to 2147483647(default 1000)"
+                "      -- the value of  rpbft_epoch_block_num from 1 to 2147483647"
                         + "(default 1000).");
         System.out.println(
                 "      -- the value of  rpbft_epoch_sealer_num " + Common.PositiveIntegerRange);
