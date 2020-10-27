db00da2678bc53a01fd69a84ded34e9463930219
cyjseagull <yujiechen_hust@163.com>
2020-10-27 18:10:57 +0800

    fix bugs when deploying contracft with params

--- src/main/java/console/contract/ConsoleContractImpl.java
+++ src/main/java/console/contract/ConsoleContractImpl.java
@@ -74,7 +74,7 @@ public class ConsoleContractImpl implements ConsoleContractFace {
     public void deploy(String[] params) throws ConsoleMessageException {
         String contractNameOrPath = params[1];
         String contractName = ConsoleUtils.getContractName(contractNameOrPath);
-        List<String> inputParams = Arrays.asList(params).subList(1, params.length - 1);
+        List<String> inputParams = Arrays.asList(params).subList(2, params.length - 1);
         deployContract(contractName, contractNameOrPath, inputParams);
     }
 
