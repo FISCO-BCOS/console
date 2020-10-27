504dc328a502469e84d3476528fcdfda8063b6f3
cyjseagull <yujiechen_hust@163.com>
2020-10-27 10:35:37 +0800

    make console depends on v2.6.1 java-sdk

--- src/main/java/console/common/ConsoleVersion.java
+++ src/main/java/console/common/ConsoleVersion.java
@@ -2,7 +2,7 @@ package console.common;
 
 public class ConsoleVersion {
 
-    public static final String Version = "2.6.0";
+    public static final String Version = "2.6.1";
 
     public static void main(String[] args) {
         System.out.println("console version: " + Version);
