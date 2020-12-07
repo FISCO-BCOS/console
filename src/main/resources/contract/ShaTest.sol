c32292a9bea6ddf5b8b9987ef6276196af152cfa
cyjseagull <yujiechen_hust@163.com>
2020-12-07 21:19:54 +0800

    add sm3/keccak256Hash interface test for CryptoPrecompiled

--- src/main/resources/contract/ShaTest.sol
+++ src/main/resources/contract/ShaTest.sol
@@ -1,8 +1,17 @@
 pragma solidity>=0.4.24 <0.6.11;
 
+pragma experimental ABIEncoderV2;
+
+import "./Crypto.sol";
+
 contract ShaTest{
     bytes _data = "Hello, ShaTest";
-    
+    Crypto crypto;
+
+    constructor() public {
+        crypto = Crypto(0x5006);
+    }
+
     function getSha256(bytes memory _memory) public returns(bytes32 result)
     {
         return sha256(_memory);
@@ -11,6 +20,16 @@ contract ShaTest{
     function getKeccak256(bytes memory _memory) public returns(bytes32 result)
     {
         return keccak256(_memory);
+    }
+
+    function calculateSM3(bytes memory _memory) public returns(bytes32 result)
+    {
+        return crypto.sm3(_memory);
+    }
+
+    function calculateKeccak256(bytes memory _memory) public returns(bytes32 result)
+    {
+        return crypto.keccak256Hash(_memory);
     }
 
     function getData() public view returns(bytes memory)
