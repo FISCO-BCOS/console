1633779e682c4c8503ac3d403dad80dbbd3492ab
cyjseagull <1547158861@qq.com>
2021-10-09 09:10:25 +0800

    print the transaction input when send transactions (#490)

--- src/main/java/console/common/ConsoleVersion.java
+++ src/main/java/console/common/ConsoleVersion.java
@@ -2,7 +2,7 @@ package console.common;
 
 public class ConsoleVersion {
 
-    public static final String Version = "2.8.0";
+    public static final String Version = "2.8.1";
 
     public static void main(String[] args) {
         System.out.println("console version: " + Version);
