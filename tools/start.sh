8dbfe61a973f4a7008fedf511112a5a8690127b0
octopus <912554887@qq.com>
2021-11-17 22:40:21 +0800

    upgrade log4j to log4j2

--- tools/start.sh
+++ tools/start.sh
@@ -65,7 +65,8 @@ elif [ "${1}" == "-v" ] || [ "${1}" == "--version" ];then
     java -cp "apps/*:conf/:lib/*:classes/" console.common.ConsoleVersion
 else
    check_java
-   java -Djdk.tls.namedGroups="secp256k1" -cp "apps/*:conf/:lib/*:classes/:accounts/" console.Console $@
-   # Note: must put the ffi library into the -Djava.library.ffipath firstly 
-   #java -Djava.library.ffipath="dyn_libs" -Djava.library.path=. -Djdk.tls.namedGroups="secp256k1" -cp "apps/*:conf/:lib/*:classes/:accounts/" console.Console $@
+   java -cp "apps/*:conf/:lib/*:classes/:accounts/" console.Console $@
+   # java -Dorg.fisco.bcos.jni.disableSsl="true" -cp "apps/*:conf/:lib/*:classes/:accounts/" console.Console $@
+   # Note: must put the ffi library into the -Djava.library.ffipath firstly
+   #java -Djava.library.ffipath="dyn_libs" -Djava.library.path=. -cp "apps/*:conf/:lib/*:classes/:accounts/" console.Console $@
 fi
