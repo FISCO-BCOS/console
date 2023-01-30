58fbb930b53e3a2a28b57d7dc37e2ee8d4b9b6cd
Kyon <32325790+kyonRay@users.noreply.github.com>
2023-01-30 16:32:34 +0800

    <feat>(command): adapt 3.3 dynamic auth check feature. (#735)

--- src/main/java/console/common/Common.java
+++ src/main/java/console/common/Common.java
@@ -11,6 +11,7 @@ public class Common {
     public static final String TxGasLimit = "tx_gas_limit";
     public static final String ConsensusLeaderPeriod = "consensus_leader_period";
     public static final String compatibilityVersion = "compatibility_version";
+    public static final String authCheckStatus = "auth_check_status";
 
     public static final int InvalidReturnNumber = -100;
     public static final long InvalidLongValue = Long.MAX_VALUE;
